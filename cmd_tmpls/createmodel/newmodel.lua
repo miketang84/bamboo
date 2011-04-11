c63b53266c0ba5f1f8e581bf9a4cf922883f86f9
Daogang Tang <daogangtang@gmail.com>
2011-04-11 17:12:56 +0800

    add createmodel command

---
+++ cmd_tmpls/createmodel/newmodel.lua
@@ -0,0 +1,27 @@
+module(..., package.seeall)
+
+local Model = require 'bamboo.model'
+
+local $MODEL = Model:extend {
+	__tag = 'Bamboo.Model.$MODEL';
+	__name = '$MODEL';
+	__desc = 'Generitic $MODEL definition';
+	__fields = {
+		{ 'name', '', false },	
+	
+	};
+	
+	init = function (self, t)
+		if not t then return self end
+		self.name = t.name or self.name
+		
+		return self
+	end;
+
+}
+
+return $MODEL
+
+
+
+
