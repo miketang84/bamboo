77f77470a46e4c97f7ac703b6460f22b17091b39
Daogang Tang <daogangtang@gmail.com>
2011-04-25 14:09:02 +0800

    modify the template inheritance finding file directory method

--- view.lua
+++ view.lua
@@ -3,13 +3,29 @@ module(..., package.seeall)
 local PLUGIN_LIST = bamboo.PLUGIN_LIST
 local G_TMPL_DIR = 'views/'
 
+local function findTemplDir( name )
+    -- 首先，找用户指定路径
+    if USERDEFINED_VIEWS and posix.access(USERDEFINED_VIEWS + name) then
+        return USERDEFINED_VIEWS
+    -- 第二，找工程下的views目录
+    elseif posix.access( APP_DIR + "views/" + name) then
+        return APP_DIR + "views/"
+    -- 第三，找工程下的plugins目录
+    elseif posix.access( APP_DIR + "plugins/" + name) then
+        return APP_DIR + "plugins/"
+    else
+        error("Template " + name + " does not exist or wrong permissions.")
+    end
+
+end
+
+
 local localvars_pattern_list = {
     -- 判断是否包含循环
     'for%s+([%w_%s,]-)%s+in',
     -- 判断是否包含local定义新变量，必须要用等于号
     'local([%w_%s,]+)=',
 }
-
 
 -- 模板渲染指令
 local VIEW_ACTIONS = {
@@ -64,7 +80,9 @@ local VIEW_ACTIONS = {
     end,
     -- 在这个函数中，传进来的code就是被继承的基页名称
     ['{:'] = function(code, this_page)
-        local base_page = io.loadFile(G_TMPL_DIR, unseri(code))
+        local name = unseri(code)
+        local tmpl_dir = findTemplDir(name)
+        local base_page = io.loadFile(tmpl_dir, name)
         local new_page = base_page
         for block in new_page:gmatch("({%[[%s_%w%.%-\'\"]+%]})") do
             -- 获取到里面的内容
@@ -124,6 +142,8 @@ local VIEW_ACTIONS = {
 }
 
 
+
+
 -- 注：此类的实例是一个View函数，用于接收表参数产生具体的页面内容。
 local View = Object:extend {
     __tag = "Bamboo.View";
@@ -136,19 +156,7 @@ local View = Object:extend {
     -- @return 一个函数 这个函数在后面的使用中接收一个table作为参数，以完成最终的模板渲染
     ------------------------------------------------------------------------
     init = function (self, name) 
-        local tmpl_dir = './'
-        -- 首先，找用户指定路径
-        if USERDEFINED_VIEWS and posix.access(USERDEFINED_VIEWS + name) then
-            tmpl_dir = USERDEFINED_VIEWS
-        -- 第二，找工程下的views目录
-        elseif posix.access( APP_DIR + "views/" + name) then
-            tmpl_dir = APP_DIR + "views/"
-        -- 第三，找工程下的plugins目录
-        elseif posix.access( APP_DIR + "plugins/" + name) then
-            tmpl_dir = APP_DIR + "plugins/"
-        else
-            error("Template " + tmpl_dir + name + " does not exist or wrong permissions.")
-        end
+        local tmpl_dir = findTemplDir(name)
         -- print('Template file dir:', tmpl_dir, name)
         
         if os.getenv('PROD') then
