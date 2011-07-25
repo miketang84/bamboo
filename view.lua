1dcedd7289813f6a324a89afee5b1a1fd1f3a4c7
Daogang Tang <daogangtang@gmail.com>
2011-07-25 16:25:00 +0800

    Change the view rendering algorithm.
    remove the css part and script part of one html page before rendering,
    and restore them back when rendering finishes.
    
    This can avoid the errors occured in some cases.
    
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- view.lua
+++ view.lua
@@ -208,46 +208,113 @@ local View = Object:extend {
     compileView = function (tmpl, name)
         local tmpl = ('%s{}'):format(tmpl)
         local code = {'local _result, _children = {}, {}\n'}
-		local loc_b, loc_e = 1, 1
+		local loc_b, loc_e = 1, 0
+		local has_script, has_css = false, false
+		
 		local text, block
 		-- :gsub("", ''):gsub("%<style%>.-%</style%>", '')
 		
-		loc_b, loc_e, script_snippet  = tmpl:find("(%<script *[%w=\'\"]*%>.-%</script%>)", loc_e)
-
+		local script_array = {}
+		local script_snippet = ''
 		while true do
-			loc_b, loc_e, text, block = tmpl:find("([^{]-)(%b{})", loc_e)
+			loc_b, loc_e, script_snippet  = tmpl:find("(%<script *[%w=\'\"]*%>.-%</script%>)", loc_e + 1)
 			if not loc_b then break end
 			
-			local script_b, script_e, script_tag = tmpl:find("%<(/?script)[ %w=\'\"]*%>", loc_e)
-			
-			if script_b and script_tag == '/script' then
-				-- skip the <script> part
-				loc_e = script_e + 1
-			else
-				local css_b, css_e, css_tag = tmpl:find("%<(/?style)%>", loc_e)
-				if css_b and css_tag == '/style' then
-					-- skip the <style> part
-					loc_e = css_e + 1
-				else
-					local act = VIEW_ACTIONS[block:sub(1,2)]
+			script_array[#script_array + 1] = script_snippet
+			tmpl = table.concat({tmpl:sub(1, loc_b - 1), '####SCRIPT_TOBEFILLED####', tmpl:sub(loc_e + 1, -1)})
+			loc_e = loc_b - 1
+		end
+		if #script_array > 0 then has_script = true end
 
-					if act then
-						code[#code+1] =  '_result[#_result+1] = [[' + text + ']]'
-						code[#code+1] = act(block:sub(3,-3))
-					elseif #block > 2 then
-						code[#code+1] = '_result[#_result+1] = [[' + text + block + ']]'
-					else
-						code[#code+1] =  '_result[#_result+1] = [[' + text + ']]'
-					end
-				end
-			end
+		-- now, script_array contains all script part of a html file
+
+		local css_array = {}
+		local css_snippet = ''
+		loc_b = 1
+		loc_e = 0
+		while true do
+			loc_b, loc_e, css_snippet  = tmpl:find("(%<style%>.-%</style%>)", loc_e + 1)
+			if not loc_b then break end
 			
+			css_array[#css_array + 1] = css_snippet
+			tmpl = table.concat({tmpl:sub(1, loc_b - 1), '####CSS_TOBEFILLED####', tmpl:sub(loc_e + 1, -1)})
+			
+			loc_e = loc_b - 1
+		end
+		if #css_array > 0 then has_css = true end
+		-- now, script_array contains all script part of a html file
+		
+		-- render the rest
+		for text, block in tmpl:gmatch("([^{]-)(%b{})") do		
+			local act = VIEW_ACTIONS[block:sub(1,2)]
+
+			if act then
+				code[#code+1] =  '_result[#_result+1] = [[' + text + ']]'
+				code[#code+1] = act(block:sub(3,-3))
+			elseif #block > 2 then
+				code[#code+1] = '_result[#_result+1] = [[' + text + block + ']]'
+			else
+				code[#code+1] =  '_result[#_result+1] = [[' + text + ']]'
+			end
 		end
 		
         code[#code+1] = 'return table.concat(_result)'
-
         code = table.concat(code, '\n')
-        local func, err = loadstring(code, name)
+        -- print(code)
+
+        -- restore the script and css part to each fragment
+        if has_script then
+			local loc_e = 1
+			local scripts_tofill = {}
+			while true do
+				loc_b, loc_e = code:find("####SCRIPT_TOBEFILLED####", loc_e + 1)
+				if not loc_b then break end
+				
+				scripts_tofill[#scripts_tofill + 1] = {loc_b, loc_e}
+			end
+			
+			local frags = {}
+			local head = 1
+			for i, v in ipairs(scripts_tofill) do
+				frags[#frags + 1] = code:sub(head, v[1] - 1)
+				frags[#frags + 1] = script_array[i] or ''
+				head = v[2] + 1
+			end
+			if head < #code then
+				frags[#frags + 1] = code:sub(head, -1)
+			end
+			code = table.concat(frags)
+		    -- print(code)
+		end
+		
+
+        -- restroe css part
+        if has_css then
+			local loc_e = 1
+			local css_tofill = {}
+			while true do
+				loc_b, loc_e = code:find("####CSS_TOBEFILLED####", loc_e + 1)
+				if not loc_b then break end
+				
+				css_tofill[#css_tofill + 1] = {loc_b, loc_e}
+			end
+			
+			local frags = {}
+			local head = 1
+			for i, v in ipairs(css_tofill) do
+				frags[#frags + 1] = code:sub(head, v[1] - 1)
+				frags[#frags + 1] = css_array[i] or ''
+				head = v[2] + 1
+			end
+			if head < #code then
+				frags[#frags + 1] = code:sub(head, -1)
+			end
+			code = table.concat(frags)
+			-- print(code)
+        end
+        
+        -- compile the whole string code
+        local func, err = loadstring(code)
 
         if err then
             assert(func, err)
