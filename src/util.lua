b8e6b26154a7fba0a952558ba58c190dbc5207bb
Daogang Tang <daogangtang@gmail.com>
2011-10-26 16:16:17 +0800

    add DEBUG function, can debug 0, 1, 2 level.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- src/util.lua
+++ src/util.lua
@@ -13,3 +13,36 @@ function simpleRedirect(rurl)
 	end
 end
 
+
+-- DEBUG level:
+-- 0, none;
+-- 1, verbose;
+-- 2, detailed;
+_G['DEBUG'] = function (...)
+	
+	local printout = function (level, ...)
+		for i = 1, select('#', ...) do
+			local arg = select (i, ...)
+			if type(arg) == 'table' then
+				if level >= 2 then
+					fptable(arg)
+				else
+					ptable(arg)
+				end
+			else
+				print(arg)
+			end
+		end
+	end
+	
+	local debug_level = bamboo.config.debug_level
+	if not isFalse(debug_level) then
+		local its_type = type(debug_level)
+		if its_type == 'boolean' then
+			printout(2, ...)
+		elseif its_type == 'number' then	
+			printout(debug_level, ...)
+		end
+	end
+		
+end	
