9cc70f3131361de7bdb26839eb7c524bd69535c5
Daogang Tang <daogangtang@gmail.com>
2013-06-18 22:36:39 +0800

    1. remove the lgserver's config file reading; 2. add a pair of cmd zmq channels for bamboo; 3. add clear_view_cache cmd to bamboo, to let it reload those view caches.
    Signed-off-by: Daogang Tang <daogangtang@gmail.com>

--- src/view.lua
+++ src/view.lua
@@ -3,14 +3,14 @@ local lgstring = require "lgstring"
 
 
 local function getlocals(context, depth)
-	local i = 1
-	while true do
-		local name, value = debug.getlocal(depth, i)
-		if not name then break end
-		context[name] = value
-		i = i + 1
-	end
-	return context
+  local i = 1
+  while true do
+    local name, value = debug.getlocal(depth, i)
+    if not name then break end
+    context[name] = value
+    i = i + 1
+  end
+  return context
 end
 
 
@@ -53,8 +53,8 @@ local VIEW_ACTIONS = {
         return true
     end,
     -- template inheritation syntax
-	-- @param code: the base file's name
-	-- @param this_page: the master file rendered
+  -- @param code: the base file's name
+  -- @param this_page: the master file rendered
     ['{:'] = function(code, this_page)
         local name = deserialize(code)
         local tmpl_dir = findTemplDir(name)
@@ -63,24 +63,24 @@ local VIEW_ACTIONS = {
         local oi, oj = 1, 0
         local i, j = 1, 0
         local block, matched, block_content
-       	local part
-       	local parts = {}
+        local part
+        local parts = {}
         while true do
-        	oi, oj, block = base_page:find("({%[[%s_%w%.%-\'\"]+%]})", oj + 1)
-        	if oi == nil then break end
+          oi, oj, block = base_page:find("({%[[%s_%w%.%-\'\"]+%]})", oj + 1)
+          if oi == nil then break end
             block_content = block:sub(3, -3):trim()
             while true do
-            	i, j, matched = this_page:find("(%b{})", j + 1)
-            	
-            	if i == nil then break end
-            	part = matched:match('^{%[%s*====*%s*' + block_content +
+              i, j, matched = this_page:find("(%b{})", j + 1)
+              
+              if i == nil then break end
+              part = matched:match('^{%[%s*====*%s*' + block_content +
             '%s*====*%s+(.+)%s*%]}$')
-				if part then
-					table.insert(parts, base_page:sub(starti, oi-1))
-					table.insert(parts, part)
-					starti = oj + 1
-					break
-				end
+        if part then
+          table.insert(parts, base_page:sub(starti, oi-1))
+          table.insert(parts, part)
+          starti = oj + 1
+          break
+        end
             end
         end
         table.insert(parts, base_page:sub(starti, -1))
@@ -110,11 +110,11 @@ local VIEW_ACTIONS = {
     end,
     
     ['{-'] = function (code)
-		return ""
+    return ""
     end,
     
     ['{<'] = function (code)
-		local code = code:trim()
+    local code = code:trim()
         assert( code ~= '', 'Widget name must not be blank.')
         local divider_loc = code:find(' ')
         local widget_name = nil
@@ -137,176 +137,174 @@ local VIEW_ACTIONS = {
 }
 
 local function inheritate(tmpl)
-	-- if there is inherited tag in page, that tag must be put in the front of this file
-	local block = tmpl:match("(%b{})")
+  -- if there is inherited tag in page, that tag must be put in the front of this file
+  local block = tmpl:match("(%b{})")
 --	local headtwo = block:sub(1,2)
-	local block_content = block:sub(3, -3)
+  local block_content = block:sub(3, -3)
 --	assert(headtwo == '{:', 'The inheriate tag must be put in front of the page.')
 
-	local act = VIEW_ACTIONS['{:']
-	return act(block_content, tmpl)
+  local act = VIEW_ACTIONS['{:']
+  return act(block_content, tmpl)
 end
             
 -- NOTE: the instance of this class is a function
 local View = Object:extend {
-    ------------------------------------------------------------------------
-    --
-    -- if config.PRODUCTION is true, means it is in production mode, it will only be compiled once every call View()
-    -- else, it is in develop mode, it will be compiled every request coming in, in compile stage and parameter fill stage.
-    -- @param name:  the name of the template file
-    -- @return:  a function, this function can receive a table to finish the rendering procedure
-    ------------------------------------------------------------------------
-    init = function (self, name, is_inline)
-        local tmpl
-	if not name then return '' end
-	
-	if bamboo.config.PRODUCTION then
-            -- if cached
-	        -- NOTE: here, 5 is an empiric value
-    	    bamboo.compiled_views_locals[name] = getlocals({}, 4)
-            
-            local view = bamboo.compiled_views[name]
-            if view and type(view) == 'function' then
-            	return view
-            end
-			
-			-- passing tmpl directly
-			if is_inline == 'inline' then
-				-- here, name is the content of html fragment
-				tmpl = name
-			else
-				-- load file
-				local tmpl_dir = findTemplDir(name)
-				tmpl = io.loadFile(tmpl_dir, name)
-            end
-			tmpl = self.preprocess(tmpl)
-            view = self.compileView(tmpl, name)
-            -- add to cache
-            bamboo.compiled_views[name] = view
-            return view
+  ------------------------------------------------------------------------
+  --
+  -- if config.PRODUCTION is true, means it is in production mode, it will only be compiled once every call View()
+  -- else, it is in develop mode, it will be compiled every request coming in, in compile stage and parameter fill stage.
+  -- @param name:  the name of the template file
+  -- @return:  a function, this function can receive a table to finish the rendering procedure
+  ------------------------------------------------------------------------
+  init = function (self, name, is_inline)
+    local tmpl
+    if not name then return '' end
+    
+    if bamboo.config.PRODUCTION then
+      -- if cached
+      -- NOTE: here, 5 is an empiric value
+      bamboo.compiled_views_locals[name] = getlocals({}, 4)
+        
+      local view = bamboo.compiled_views[name]
+      if view and type(view) == 'function' then
+        return view
+      end
+  
+      -- passing tmpl directly
+      if is_inline == 'inline' then
+        -- here, name is the content of html fragment
+        tmpl = name
+      else
+        -- load file
+        local tmpl_dir = findTemplDir(name)
+        tmpl = io.loadFile(tmpl_dir, name)
+      end
+      tmpl = self.preprocess(tmpl)
+      view = self.compileView(tmpl, name)
+      -- add to cache
+      bamboo.compiled_views[name] = view
+      return view
+    else
+      return function (params)
+        -- passing tmpl directly
+        if is_inline == 'inline' then
+          -- here, name is the content of html fragment
+          tmpl = name
         else
-            return function (params)
-				-- passing tmpl directly
-				if is_inline == 'inline' then
-					-- here, name is the content of html fragment
-					tmpl = name
-				else
-					local tmpl_dir = findTemplDir(name)
-					tmpl = io.loadFile(tmpl_dir, name)
-					assert(tmpl, "Template " + tmpl_dir + name + " does not exist.")
-				end
-                
-                tmpl = self.preprocess(tmpl)
-                return self.compileView(tmpl, name)(params)
+          local tmpl_dir = findTemplDir(name)
+          tmpl = io.loadFile(tmpl_dir, name)
+          assert(tmpl, "Template " + tmpl_dir + name + " does not exist.")
+        end
+        tmpl = self.preprocess(tmpl)
+        return self.compileView(tmpl, name)(params)
+      end
+    end
+  end;
+
+  -- preprocess course
+  preprocess = function(tmpl)
+
+    -- restrict the {: :} at the head of template file, from the first char
+    while tmpl:match('^{:.-:}%s*\n') do
+      tmpl = inheritate(tmpl)
+    end
+
+    tmpl = tmpl:gsub('%[%[%s*====*%s*([\'\"%w_%-]+)%s*====*%s+(.-)%s*====*%s*%]%]', function (skey, sval)
+      local firstchar = skey:sub(1,1)
+      if firstchar == "'" or firstchar == '"' then skey = skey:sub(2,-2) end
+      bamboo.context[skey] = sval
+      
+      return ''
+    end)
+    return tmpl
+  end;
+
+  ------------------------------------------------------------------------
+  -- compile template string to a middle function
+  -- use this function to receive a table to finish rendering to html
+  --
+  -- this snippet is very concise and powerful!
+  -- @param tmpl:  template string read from file
+  -- @param name:  template file name
+  -- @return: middle rendering function
+  ------------------------------------------------------------------------
+  compileView = function (tmpl, name)
+    local tmpl = ('%s{{""}}'):format(tmpl)
+    local code = {'local _result, _children = {}, {}\n'}
+
+    -- render the rest
+    local text, block
+    for text, block in lgstring.matchtagset(tmpl) do
+      local act = VIEW_ACTIONS[block:sub(1,2)]
+
+      if act then
+        code[#code+1] = '_result[#_result+1] = [==[' + text + ']==]'
+        code[#code+1] = act(block:sub(3,-3))
+      elseif #block > 2 then
+        code[#code+1] = '_result[#_result+1] = [==[' + text + block + ']==]'
+      else
+        code[#code+1] = '_result[#_result+1] = [==[' + text + ']==]'
+      end
+    end
+
+    -- do check before concatate
+    -- if code[i] is nil, it won't be countered
+    -- we must find all viewing bugs in develop mode
+    code[#code+1] = [==[ 
+      if not bamboo.config.PRODUCTION then
+        local ctype
+        for i, v in ipairs(_result) do  
+          local ctype = type(v)
+          if ctype ~= 'string' or ctype ~= 'number' then
+            _result[i] = tostring(v)
+          end
+        end 
+      end 
+    ]==]
+    code[#code+1] = 'return table.concat(_result)'
+    code = table.concat(code, '\n')
+    -- print('-----', code)
+    -- recode each middle view code to request
+    if type(name) == 'string' then
+      bamboo.compiled_views_tmpls[name] = code
+    end
+
+    -- compile the whole string code
+    local func, err = loadstring(code, name)
+
+    if err then
+        assert(func, err)
+    end
+
+    return function(context)
+      assert(type(context) == 'table', "You must always pass in a table for context.")
+      -- collect locals
+      if context[1] == 'locals' then  
+        context[1] = nil
+        if bamboo.config.PRODUCTION then
+          local locals = bamboo.compiled_views_locals[name]
+          if locals then
+            for k, v in pairs(locals) do
+              if not context[k] then context[k] = v end
             end
+          end
+        else
+          -- NOTE: here, 4 is empiric value
+          context = getlocals(context, 3)
         end
-
-    end;
-
-	-- preprocess course
-	preprocess = function(tmpl)
-
-		-- restrict the {: :} at the head of template file, from the first char
-		while tmpl:match('^{:.-:}%s*\n') do
-            tmpl = inheritate(tmpl)
+      end
+      
+      -- for global context rendering
+      for k, v in pairs(bamboo.context) do
+        if not context[k] then
+          context[k] = v
         end
-
-		tmpl = tmpl:gsub('%[%[%s*====*%s*([\'\"%w_%-]+)%s*====*%s+(.-)%s*====*%s*%]%]', function (skey, sval)
-			local firstchar = skey:sub(1,1)
-			if firstchar == "'" or firstchar == '"' then skey = skey:sub(2,-2) end
-			bamboo.context[skey] = sval
-			
-			return ''
-		end)
-		return tmpl
-    end;
-
-    ------------------------------------------------------------------------
-    -- compile template string to a middle function
-    -- use this function to receive a table to finish rendering to html
-    --
-	-- this snippet is very concise and powerful!
-    -- @param tmpl:  template string read from file
-    -- @param name:  template file name
-    -- @return: middle rendering function
-    ------------------------------------------------------------------------
-    compileView = function (tmpl, name)
-        local tmpl = ('%s{{""}}'):format(tmpl)
-        local code = {'local _result, _children = {}, {}\n'}
-
-		-- render the rest
-		local text, block
-		for text, block in lgstring.matchtagset(tmpl) do
-			local act = VIEW_ACTIONS[block:sub(1,2)]
-
-			if act then
-				code[#code+1] = '_result[#_result+1] = [==[' + text + ']==]'
-				code[#code+1] = act(block:sub(3,-3))
-			elseif #block > 2 then
-				code[#code+1] = '_result[#_result+1] = [==[' + text + block + ']==]'
-			else
-				code[#code+1] = '_result[#_result+1] = [==[' + text + ']==]'
-			end
-		end
-
-		-- do check before concatate
-		-- if code[i] is nil, it won't be countered
-		-- we must find all viewing bugs in develop mode
-		code[#code+1] = [==[ 
-			if not bamboo.config.PRODUCTION then
-				local ctype
-				for i, v in ipairs(_result) do  
-					local ctype = type(v)
-					if ctype ~= 'string' or ctype ~= 'number' then
-						_result[i] = tostring(v)
-					end
-				end 
-			end 
-		]==]
-        code[#code+1] = 'return table.concat(_result)'
-        code = table.concat(code, '\n')
-        -- print('-----', code)
-        -- recode each middle view code to request
-        if type(name) == 'string' then
-        	bamboo.compiled_views_tmpls[name] = code
-		end
-
-        -- compile the whole string code
-        local func, err = loadstring(code, name)
-
-        if err then
-            assert(func, err)
-        end
-
-        return function(context)
-            assert(type(context) == 'table', "You must always pass in a table for context.")
-			-- collect locals
-			if context[1] == 'locals' then  
-				context[1] = nil
-				if bamboo.config.PRODUCTION then
-					local locals = bamboo.compiled_views_locals[name]
-					if locals then
-						for k, v in pairs(locals) do
-							if not context[k] then context[k] = v end
-						end
-					end
-				else
-					-- NOTE: here, 4 is empiric value
-					context = getlocals(context, 3)
-				end
-			end
-			
-			-- for global context rendering
-			for k, v in pairs(bamboo.context) do
-				if not context[k] then
-					context[k] = v
-				end
-			end
-			setmetatable(context, {__index=_G})
-			setfenv(func, context)
-			return func()
-        end
-    end;
+      end
+      setmetatable(context, {__index=_G})
+      setfenv(func, context)
+      return func()
+    end
+  end;
 }
 
 return View
