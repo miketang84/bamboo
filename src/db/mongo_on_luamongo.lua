15b9d0456d8d78319bb62f963170bf1f7f9cc3cc
Daogang Tang <daogangtang@126.com>
2014-12-04 15:30:35 +0800

    update mongo.lua to mongo_on_luamongo.lua.
    Signed-off-by: Daogang Tang <daogangtang@126.com>

--- src/db/mongo_on_luamongo.lua
+++ src/db/mongo_on_luamongo.lua
@@ -6,6 +6,10 @@ module(..., package.seeall)
 
 local mongo = require "mongo"
 
+local QueryOption_SlaveOk = 4
+
+
+
 
 
 local function cursor_all(cursor)
@@ -33,6 +37,15 @@ end
 local find = function (self, ns, query, fieldsToReturn, nToSkip, nToReturn, queryOptions, batchSize)
     assert(type(ns) == 'string')
     
+    if self.secondary then
+        local bit = require("bit")
+        if queryOptions then
+            queryOptions = bit.bor(queryOptions, QueryOption_SlaveOk)
+        else
+            queryOptions = QueryOption_SlaveOk
+        end
+    end
+
     local cursor, err = self.conn:query(self.db..'.'..ns, query, nToReturn, nToSkip, fieldsToReturn, queryOptions, batchSize)
     if err then print(err) end
 
@@ -42,9 +55,9 @@ local find = function (self, ns, query, fieldsToReturn, nToSkip, nToReturn, quer
 
 end
 
-local findOne = function (self, ns, query, fieldsToReturn, nToSkip)
+local findOne = function (self, ns, query, fieldsToReturn, nToSkip, queryOptions, batchSize)
     
-    local cursor = find(self, ns, query, fieldsToReturn, nToSkip)
+    local cursor = find(self, ns, query, fieldsToReturn, nToSkip, queryOptions, batchSize)
     local result = cursor:next()
 
     -- XXX: need to notice whether it is nil when none found
@@ -127,8 +140,9 @@ function _connect(config)
     assert( conn ~= nil, 'unable to create mongo.Connection' )
     assert( conn:connect(conn_str), 'unable to forcefully connect to mongo instance' )
 
-    if config.user then
-        assert( conn:auth { dbname = 'admin', username = config.user, password = config.password } == true, "unable to auth to db" )
+    if config.user and config.user ~= '' then
+        --assert( conn:auth { dbname = 'admin', username = config.user, password = config.password } == true, "unable to auth to db" )
+        assert( conn:auth { dbname = dbname, username = config.user, password = config.password } == true, "unable to auth to db" )
     end
 
     return conn
@@ -136,22 +150,24 @@ end
 
 function _connect_replica_set(config)
     assert(type(config) == 'table', 'missing config in mongo.connect')
+    local set = config.set
+    assert(set, 'missing set in replica set connect.')
     local dbname = config.db
     -- ensure replicaset is a host:port array
     local replicaset = config.replicaset
 
     -- Create a Connection object
-    local conn = mongo.ReplicaSet.New(dbname, replicaset)
+    local conn = mongo.ReplicaSet.New(set, replicaset)
     assert( conn ~= nil, 'unable to create mongo.ReplicaSetConnection' )
     assert( conn:connect(), 'unable to forcefully connect to mongo instance' )
-    
-    print(conn, type(conn))
-    print(config.user)
-    print(config.password)
+   
+	print(conn)
 
-    if config.user then
-        assert( conn:auth { dbname = 'admin', username = config.user, password = config.password } == true, "unable to auth to db" )
+    if config.user and config.user ~= '' then
+        --assert( conn:auth { dbname = 'admin', username = config.user, password = config.password } == true, "unable to auth to db" )
+        assert( conn:auth { dbname = dbname, username = config.user, password = config.password } == true, "unable to auth to db" )
     end
+	print('db ', dbname, config.user, config.password, 'authored success.')
 
     return conn
 end
@@ -172,12 +188,9 @@ local _mt_db = {
 
 function connect (config)
     assert(type(config) == 'table', 'missing config in mongo.connect')
-    config.host = config.host or '127.0.0.1'
-    config.port = config.port or '27017'
     config.db = config.db or 'test'
     local replicaset = config.replicaset
     
-
     local db = {}
     -- attach all methods to db
     setmetatable(db, { __index = _mt_db })
@@ -185,6 +198,8 @@ function connect (config)
     if replicaset then
         conn = _connect_replica_set(config)
     else
+		config.host = config.host or '127.0.0.1'
+		config.port = config.port or '27017'
         conn = _connect(config)
     end
     
@@ -196,3 +211,59 @@ function connect (config)
     
     return db
 end
+
+function connectReplSet(config, username, password, secondary)
+	assert(config, "connectReplSet config null");
+	assert(config.primary, "connectReplSet config.primary");
+
+	local primary_str = config.primary.host..':'..config.primary.port
+	local second_strs = {}
+	if config.secondaries then
+		for i, s in ipairs(config.secondaries) do
+			second_strs[i] = s.host..':'..s.port
+		end
+	end
+
+	local replicaset = {primary_str}	
+	for i=1, #second_strs do
+		table.insert(replicaset, second_strs[i])
+	end
+
+	local newconfig = {
+        set = config.set,
+		db = config.dbname,
+		user = username,
+		password = password,
+		replicaset = replicaset,
+        secondary = secondary
+	}
+
+	return connect(newconfig)
+end
+
+
+function connectPrimary(config, username, password)
+	assert(config, "connectReplSet config null");
+	assert(config.primary, "connectReplSet config.primary");
+
+
+	local newconfig = {
+		host = config.primary.host,
+		port = config.primary.port,
+		db = config.dbname,
+		user = username,
+		password = password
+	}
+
+	return connect(newconfig)
+end
+
+
+function connectSecondary(config, username, password)
+	assert(config, "connectReplSet config null");
+	assert(config.primary, "connectReplSet config.primary");
+
+    return connectReplSet(config, username, password, true);
+end
+
+
